552d63ee450a5c7c4f161d26f2970b92e9aeeb5b
Luiz Guilherme <88911920+luizgmelo@users.noreply.github.com>
2024-11-20 15:10:00 -0300

    Initial Commit

---
+++ src/main/java/com/shaulin/crud/controller/ProductController.java
@@ -0,0 +1,66 @@
+package com.shaulin.crud.controller;
+
+import com.shaulin.crud.dto.ProductDto;
+import com.shaulin.crud.model.Product;
+import com.shaulin.crud.repository.ProductRepository;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/products")
+public class ProductController {
+
+    @Autowired
+    ProductRepository repository;
+
+    @GetMapping
+    public ResponseEntity getAll() {
+        List<Product> listProducts = repository.findAll();
+        return ResponseEntity.status(HttpStatus.OK).body(listProducts);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity getById(@PathVariable(value = "id") Integer id) {
+        Optional<Product> product = repository.findById(id);
+        if (product.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Product not found");
+        }
+        return ResponseEntity.status(HttpStatus.OK).body(product.get());
+    }
+
+    @PostMapping
+    public ResponseEntity createProduct(@RequestBody ProductDto dto) {
+        var product = new Product();
+        BeanUtils.copyProperties(dto, product);
+        return ResponseEntity.status(HttpStatus.CREATED).body(repository.save(product));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity update(@PathVariable(value = "id") Integer id, @RequestBody ProductDto dto) {
+        Optional<Product> product = repository.findById(id);
+        if (product.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Product not found");
+        }
+        var productModel = product.get();
+        BeanUtils.copyProperties(dto, productModel);
+        repository.save(productModel);
+        return ResponseEntity.status(HttpStatus.OK).body(productModel);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity delete(@PathVariable(value = "id") Integer id) {
+        Optional<Product> product = repository.findById(id);
+        if (product.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Product not found");
+        }
+        repository.delete(product.get());
+        return ResponseEntity.status(HttpStatus.OK).body("Product deleted");
+    }
+
+}
---
+++ src/main/java/com/shaulin/crud/dto/ProductDto.java
@@ -0,0 +1,4 @@
+package com.shaulin.crud.dto;
+
+public record ProductDto(String name, Long price) {
+}
